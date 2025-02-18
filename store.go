b777372adf7badd4dedba1ba67e65dd677082b5f
Dorian González <dorianlgs@gmail.com>
2025-02-18 18:25:06 -0500

    Update store.go

--- store.go
+++ store.go
@@ -5,13 +5,14 @@ import (
 	"encoding/base64"
 
 	"github.com/go-webauthn/webauthn/webauthn"
+	"github.com/pocketbase/pocketbase/tools/store"
 )
 
 type InMem struct {
 	// TODO: it would be nice to have a mutex here
 	// TODO: use pointers to avoid copying
-	users    map[string]PasskeyUser
-	sessions map[string]webauthn.SessionData
+	users    *store.Store[string, PasskeyUser]
+	sessions *store.Store[string, webauthn.SessionData]
 
 	log Logger
 }
@@ -28,46 +29,47 @@ func (i *InMem) GenSessionID() (string, error) {
 }
 
 func NewInMem(log Logger) *InMem {
+
 	return &InMem{
-		users:    make(map[string]PasskeyUser),
-		sessions: make(map[string]webauthn.SessionData),
+		users:    store.New[string, PasskeyUser](nil),
+		sessions: store.New[string, webauthn.SessionData](nil),
 		log:      log,
 	}
 }
 
 func (i *InMem) GetSession(token string) (webauthn.SessionData, bool) {
-	i.log.Printf("[DEBUG] GetSession: %v", i.sessions[token])
-	val, ok := i.sessions[token]
+	i.log.Printf("[DEBUG] GetSession: %v", i.sessions.Get(token))
+	val, ok := i.sessions.GetOk(token)
 
 	return val, ok
 }
 
 func (i *InMem) SaveSession(token string, data webauthn.SessionData) {
 	i.log.Printf("[DEBUG] SaveSession: %s - %v", token, data)
-	i.sessions[token] = data
+	i.sessions.Set(token, data)
 }
 
 func (i *InMem) DeleteSession(token string) {
 	i.log.Printf("[DEBUG] DeleteSession: %v", token)
-	delete(i.sessions, token)
+	i.sessions.Remove(token)
 }
 
 func (i *InMem) GetOrCreateUser(userName string) PasskeyUser {
 	i.log.Printf("[DEBUG] GetOrCreateUser: %v", userName)
-	if _, ok := i.users[userName]; !ok {
+	if _, ok := i.users.GetOk(userName); !ok {
 		i.log.Printf("[DEBUG] GetOrCreateUser: creating new user: %v", userName)
-		i.users[userName] = &User{
+		i.users.Set(userName, &User{
 			ID:          []byte(userName),
 			DisplayName: userName,
 			Name:        userName,
-		}
+		})
 	}
 
-	return i.users[userName]
+	return i.users.Get(userName)
 }
 
 func (i *InMem) SaveUser(user PasskeyUser) {
 	i.log.Printf("[DEBUG] SaveUser: %v", user.WebAuthnName())
 	i.log.Printf("[DEBUG] SaveUser: %v", user)
-	i.users[user.WebAuthnName()] = user
+	i.users.Set(user.WebAuthnName(), user)
 }
